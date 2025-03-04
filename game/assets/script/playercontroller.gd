ccf6b668aee5a3d40c4754fffedb390f05b80d7d
gabriel galiana jaime <162907886+gabriel-galiana-jaime@users.noreply.github.com>
2025-03-04 19:40:44 +0100

    new movement

--- game/assets/script/playercontroller.gd
+++ game/assets/script/playercontroller.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
-var input = Vector2(0,0)
+var input_axis = Vector2(0,0)
 
 func _process(delta: float) -> void:
-	input = Input.get_action_raw_strength("right") - Input.get_action_raw_strength("left")
-	velocity.x = input * 6000 * delta
+	input_axis = input_axis_()
+	velocity = input_axis * 6000 * delta
 	move_and_slide()
 	pass
+
+func input_axis_():
+	return Vector2(Input.get_action_raw_strength("right") - Input.get_action_raw_strength("left"),Input.get_action_raw_strength("down") - Input.get_action_raw_strength("up"))
