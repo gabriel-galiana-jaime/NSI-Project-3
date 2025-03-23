8e1639b1ac0165bb9503051aa8bb166764c8247d
gabriel galiana jaime <162907886+gabriel-galiana-jaime@users.noreply.github.com>
2025-03-23 16:54:29 +0100

    new scene overworld 2 and changes

--- game/assets/script/autoload/auto_scene_index.gd
+++ game/assets/script/autoload/auto_scene_index.gd
@@ -5,4 +5,5 @@ var scene_index : Dictionary = {
 	
 	"house1":"res://assets/scene/gameplay_scene/maps/indoor/house_1.tscn",
 	"outdoor1":"res://assets/scene/gameplay_scene/maps/overworld/overworldvillage.tscn",
+	"outdoor2":"res://assets/scene/gameplay_scene/maps/overworld/overworld_2.tscn",
 }
