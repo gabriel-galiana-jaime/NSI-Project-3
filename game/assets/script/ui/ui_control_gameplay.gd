a27f903c0e6a3ab37cb6fbd021e7c59663287039
gabriel galiana jaime <162907886+gabriel-galiana-jaime@users.noreply.github.com>
2025-03-24 16:38:17 +0100

    new overworld 2 update, polishement and tileset finished

--- game/assets/script/ui/ui_control_gameplay.gd
+++ game/assets/script/ui/ui_control_gameplay.gd
@@ -2,6 +2,10 @@ extends Control
 
 
 @onready var ui_health = $"../control_2d_ui/heart_animation"
+func _ready() -> void:
+	$"..".show()
+	pass
+
 func _process(delta: float) -> void:
 	#print(str(auto_main_autoload_script.player_data["health"]))
 	ui_health.play("heart_"+ str(auto_main_autoload_script.player_health))
