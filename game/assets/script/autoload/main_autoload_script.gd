43855d7a25e846b3ad2a323cf86c8a0daea64f46
gabriel galiana jaime <162907886+gabriel-galiana-jaime@users.noreply.github.com>
2025-03-24 18:00:23 +0100

    octorog  code

--- game/assets/script/autoload/main_autoload_script.gd
+++ game/assets/script/autoload/main_autoload_script.gd
@@ -8,7 +8,7 @@ func round_position(position : Vector2):
 
 #runtimes stuff
 
-var global_spawn_player_position : Vector2
+var global_spawn_player_position : Vector2 = Vector2(-41.0,157)
 
 var player_data : Dictionary
 var player_max_health : float = 3
--- game/assets/script/npc/enemys/scr_enemy_2.gd
+++ game/assets/script/npc/enemys/scr_enemy_2.gd
@@ -1,1 +1,17 @@
 extends CharacterBody2D
+
+var direction : Vector2 = Vector2(0,-1)
+var speed : float = 900
+var raycast_len : float = 9
+func _process(delta: float) -> void:
+	velocity = basic_move_patrol(delta)
+	$RayCast2D.target_position = Vector2(raycast_len,raycast_len) * direction
+	move_and_slide()
+	
+	if $RayCast2D.is_colliding():
+		print("wall_touched")
+	pass
+
+
+func basic_move_patrol(delta):
+	return speed * direction * delta
