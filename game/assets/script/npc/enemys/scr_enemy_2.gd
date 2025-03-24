ce7d2dbbc17740536d954ef6d215065e8fff7203
gabriel galiana jaime <162907886+gabriel-galiana-jaime@users.noreply.github.com>
2025-03-24 18:51:08 +0100

    Update scr_enemy_2.gd

--- game/assets/script/npc/enemys/scr_enemy_2.gd
+++ game/assets/script/npc/enemys/scr_enemy_2.gd
@@ -5,7 +5,7 @@ var speed : float = 900
 var raycast_len : float = 9
 func _process(delta: float) -> void:
 	velocity = basic_move_patrol(delta)
-	$RayCast2D.target_position = Vector2(raycast_len,raycast_len) * direction
+	#$RayCast2D.target_position = Vector2(raycast_len,raycast_len) * direction
 	move_and_slide()
 	
 	if $RayCast2D.is_colliding():
