04e29660187c2cdf5c54e945731ca6311242677a
gabriel galiana jaime <162907886+gabriel-galiana-jaime@users.noreply.github.com>
2025-03-25 19:31:57 +0100

    fixing bug with the display heart

--- game/assets/script/ui/ui_control_gameplay.gd
+++ game/assets/script/ui/ui_control_gameplay.gd
@@ -3,6 +3,7 @@ extends Control
 
 @onready var ui_health = $"../control_2d_ui/heart_animation"
 func _ready() -> void:
+	ui_health.play("heart_"+ str(auto_main_autoload_script.player_health))
 	$"..".show()
 	pass
 
