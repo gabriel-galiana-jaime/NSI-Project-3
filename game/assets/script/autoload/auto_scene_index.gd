63a10d729c0e2f04c6346ad37605a5b327219b32
Zagel_dev <162181267+Zagel-dev@users.noreply.github.com>
2025-04-16 10:50:38 +0200

    Connected overworld3

--- game/assets/script/autoload/auto_scene_index.gd
+++ game/assets/script/autoload/auto_scene_index.gd
@@ -6,4 +6,5 @@ var scene_index : Dictionary = {
 	"house1":"res://assets/scene/gameplay_scene/maps/indoor/house_1.tscn",
 	"outdoor1":"res://assets/scene/gameplay_scene/maps/overworld/overworldvillage.tscn",
 	"outdoor2":"res://assets/scene/gameplay_scene/maps/overworld/overworld_2.tscn",
+	"outdoor3":"res://assets/scene/gameplay_scene/maps/overworld/overworld_3.tscn"
 }
